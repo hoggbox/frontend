d45310d4c918d9a2d44d3dd4a49dcaa1e15ecb91
hoggbox <94619876+hoggbox@users.noreply.github.com>
2025-04-03 10:15:07 -0400

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: data.icon
+  });
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
